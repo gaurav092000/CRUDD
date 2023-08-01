d127ba544e204a6c1589abcd09936cde3adc9413
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-08-01 18:34:44 +0530

    Add project files.

---
+++ CRUD/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        ProductContext pr=new ProductContext();
+        public ActionResult Index()
+        {
+            var data=pr.products.ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p) 
+        {
+            pr.products.Add(p);
+            int a=pr.SaveChanges();
+            if(a > 0)
+            {
+                ViewBag.InsertMessage = "<script>alert('')</script>";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            pr.products.AddOrUpdate(p);
+            pr.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        
+    }
+}
---
+++ CRUD/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class Product
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Quatity { get; set; }
+
+        public float Price { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public bool IsUpdated { get; set; }
+
+    }
+}
